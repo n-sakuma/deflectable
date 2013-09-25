10175e4e8ed24d573457214d37634d60a689f437
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-09-25 21:21:59 +0900

    Add spec_helper.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'rspec'
+require 'rack/test'
+require 'rack/mock'
+require 'deflectable'
+# require 'mocha'
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  # config.mock_framework = :mocha
+
+  config.include Rack::Test::Methods
+end
+
