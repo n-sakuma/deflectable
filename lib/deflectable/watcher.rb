9e40c7756cd825eb6283b42f46a4254920243cf5
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-09-26 00:59:29 +0900

    refactor log output.

--- lib/deflectable/watcher.rb
+++ lib/deflectable/watcher.rb
@@ -1,3 +1,4 @@
+require 'logger'
 module Deflectable
   class Watcher
     attr_accessor :options
@@ -9,6 +10,7 @@ module Deflectable
       conf = YAML.load_file(Rails.root.join('config/deflect.yml')) rescue {}
       @options = {
         :log => false,
+        :logger => ::Logger.new('deflector.log'),
         :log_format => 'deflect(%s): %s',
         :log_date_format => '%m/%d/%Y',
         :whitelist => [],
@@ -18,13 +20,14 @@ module Deflectable
     end
 
     def call(env)
-      return reject! if detect?(env)
+      return reject!(env) if detect?(env)
       status, headers, body = @app.call(env)
       [status, headers, body]
     end
 
 
     private
+
 
     def configure_check!
       if (not options[:whitelist].empty?) and (not options[:blacklist].empty?)
@@ -37,7 +40,7 @@ There was both a :blanklist and :whitelist.
       @filtering = :blacklist unless options[:blacklist].empty?
     end
 
-    def reject!
+    def reject!(env)
       res = Rack::Response.new do |r|
         r.status = 403
         r['Content-Type'] = 'text/html;charset=utf-8'
@@ -75,9 +78,13 @@ There was both a :blanklist and :whitelist.
       options[:blacklist].include?(env['REMOTE_ADDR']) ? true : false
     end
 
-    def log message
+    def log(message, level = :info)
       return unless options[:log]
-      options[:log].puts(options[:log_format] % [Time.now.strftime(options[:log_date_format]), message])
+      if logger = options[:logger]
+        logger.send(level, message)
+      else
+        puts(options[:log_format] % [Time.now.strftime(options[:log_date_format]), message])
+      end
     end
   end
 end
